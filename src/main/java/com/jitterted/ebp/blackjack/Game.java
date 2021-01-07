993027c11ae1ff6eaaf9cad56af859d5b93eab74
Ted M. Young <tedyoung@gmail.com>
2021-01-06 22:29:01 -0800

    Initial Player-Game betting implemented for player wins.

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -12,6 +12,8 @@ public class Game {
 
   private final Hand dealerHand = new Hand();
   private final Hand playerHand = new Hand();
+  private int playerBalance = 0;
+  private int playerBetAmount = 0;
 
   public static void main(String[] args) {
     displayWelcomeScreen();
@@ -66,14 +68,19 @@ public class Game {
   private void displayOutcome(boolean playerBusted) {
     if (playerBusted) {
       System.out.println("You Busted, so you lose.  💸");
+//      playerLoses();
     } else if (dealerHand.isBusted()) {
       System.out.println("Dealer went BUST, Player wins! Yay for you!! 💵");
+//      playerWins();
     } else if (playerHand.beats(dealerHand)) {
       System.out.println("You beat the Dealer! 💵");
+//      playerWins();
     } else if (playerHand.pushes(dealerHand)) {
       System.out.println("Push: The house wins, you Lose. 💸");
+//      playerPushes();
     } else {
       System.out.println("You lost to the Dealer. 💸");
+//      playerLoses();
     }
   }
 
@@ -161,4 +168,20 @@ public class Game {
     System.out.println(" (" + playerHand.displayValue() + ")");
   }
 
+  public int playerBalance() {
+    return playerBalance;
+  }
+
+  public void playerDeposits(int amount) {
+    playerBalance += amount;
+  }
+
+  public void playerBets(int betAmount) {
+    playerBalance -= betAmount;
+    playerBetAmount = betAmount;
+  }
+
+  public void playerWins() {
+    playerBalance += playerBetAmount * 2;
+  }
 }
